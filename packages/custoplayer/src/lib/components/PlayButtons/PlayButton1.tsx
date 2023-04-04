f49d8463f8443a3499a93c7f274977d9eef42f61
Etesam913 <etesamansari13@gmail.com>
2023-04-04 16:09:38 -0400

    💄 Revised playButton1 & playButton2
    ♻️ Refactored Pause & Play button file locations

--- packages/custoplayer/src/lib/components/PlayButtons/PlayButton1.tsx
+++ packages/custoplayer/src/lib/components/PlayButtons/PlayButton1.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
-function PlayButton2() {
+function PlayButton1() {
   return (
     <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <PlayButtonPath d="M10 25.5V6L26 16.2632L10 25.5Z" />
+      <PlayButtonPath d="M22.2468 17.6645L11.8588 24.5847C10.5297 25.4701 8.75 24.5173 8.75 22.9202L8.75 9.0798C8.75 7.48272 10.5297 6.52988 11.8588 7.41533L22.2468 14.3355C23.435 15.1271 23.435 16.8729 22.2468 17.6645Z" />
     </svg>
   );
 }
@@ -14,4 +14,4 @@ const PlayButtonPath = styled.path`
       stroke-linecap: round;
 `;
 
-export default PlayButton2
+export default PlayButton1
