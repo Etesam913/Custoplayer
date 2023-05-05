36251a0f9b0eec0de3525913bbcde7f74f55adff
Etesam913 <etesamansari13@gmail.com>
2023-05-04 20:39:29 -0400

    🐛 Fixed controlsBar hidden bug

--- packages/custoplayer/src/lib/components/ControlsBar.tsx
+++ packages/custoplayer/src/lib/components/ControlsBar.tsx
@@ -69,7 +69,7 @@ function ControlsBar() {
 
   return (
     <AnimatePresence>
-      {(true || isVolumeDragging || isControlsBarShowing) && (
+      {(isProgressDragging || isVolumeDragging || isControlsBarShowing) && (
         <ControlsContainer
           className={draggableSymbol.toString()}
           variants={
