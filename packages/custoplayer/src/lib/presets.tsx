7df37410b04058042003c4670564b2b31a93e0b0
Etesam913 <etesamansari13@gmail.com>
2023-05-29 19:58:23 -0400

    :lipstick: Added new theme, milkyWhite

--- packages/custoplayer/src/lib/presets.tsx
+++ packages/custoplayer/src/lib/presets.tsx
@@ -82,3 +82,48 @@ export const fieryRed: CustoplayerValues = {
   },
   focusColor: '#ffd3d3',
 };
+
+export const milkyWhite: CustoplayerValues = {
+  previewTooltip: {
+    id: 'text',
+  },
+  controlsBar: {
+    animate: 'movement',
+    barColor: '#ffffffdd',
+  },
+  item1: {
+    id: 'playButton1',
+    buttonColor: '#353535',
+  },
+  item2: {
+    id: 'volumeButton1',
+    barId: 'volumeBar2',
+    buttonColor: '#353535',
+    barColor: '#e7e7e7',
+    volumeColor: '#353535',
+    scrubberBorderColor: 'transparent',
+    scrubberColor: 'transparent',
+  },
+  item3: {
+    id: 'currentTime',
+    textColor: '#353535',
+  },
+  topProgressBar: {
+    id: 'progressBar1',
+    barColor: '#e7e7e7',
+    progressColor: '#353535',
+    scrubberColor: 'transparent',
+    scrubberBorderColor: 'none',
+  },
+  item5: {
+    id: 'duration',
+    textColor: '#353535',
+    marginLeft: 'auto',
+  },
+
+  item7: {
+    id: 'fullscreenButton1',
+    buttonColor: '#353535',
+  },
+  focusColor: '#353535',
+};
