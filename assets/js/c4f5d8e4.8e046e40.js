0f951fe666f5639cbb3c2fa414bdfa42eef9bd5d
Etesam913 <etesamansari13@gmail.com>
2023-06-07 18:57:27 -0400

    Deploy website - based on 16762532767df0bb72b95bca7df81398379e6ba0

---
+++ assets/js/c4f5d8e4.8e046e40.js
@@ -0,0 +1,37 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[195],{1104:(e,t,n)=>{n.r(t),n.d(t,{default:()=>u});var a=n(2784),r=n(2896),l=n(5837),i=n(230),o=n(966),c=n(2028),m=n(2194);function s(){const{siteConfig:e}=(0,l.Z)();return a.createElement(p,null,a.createElement("div",{className:"container"},a.createElement(h,null,e.title),a.createElement(E,null,e.tagline),a.createElement("button",{className:"link-button"},a.createElement(r.Z,{to:"/docs/quick-start"},"Learn More"))))}const d={midnightBlue:m.NT,fieryRed:m.FF,milkyWhite:m.L2,mint:m.DT,grassyGreen:m.Pv};function u(){const[e,t]=(0,a.useState)("midnightBlue");return a.createElement(i.Z,{title:"Custoplayer",description:"A React.js npm package that allows for the rapid creation of customizable video players."},a.createElement("div",null,a.createElement(s,null),a.createElement(g,null,a.createElement(f,null,a.createElement(m.O8,{poster:"https://custoplayer.nyc3.cdn.digitaloceanspaces.com/docs/custoplayer-demo-poster.png",src:"https://custoplayer.nyc3.cdn.digitaloceanspaces.com/docs/compressed-custoplayer-demo.mp4",values:d[e]})),a.createElement(y,{htmlFor:"preset-dropdown"},"Presets"),a.createElement(v,{id:"preset-dropdown",value:e,onChange:e=>t(e.target.value)},a.createElement("option",{value:"midnightBlue"},"\ud83c\udf15 Midnight Blue"),a.createElement("option",{value:"fieryRed"},"\ud83d\udd25 Fiery Red"),a.createElement("option",{value:"milkyWhite"},"\ud83e\udd5b Milky White"),a.createElement("option",{value:"grassyGreen"},"\ud83e\udd57 Grassy Green"),a.createElement("option",{value:"mint"},"\ud83c\udf3f Mint")))))}const p=(0,o.ZP)(c.E.header)`
+  text-align: center;
+  padding: 2.5rem;
+`,g=o.ZP.main`
+  padding: 0.25rem 0 1.5rem 0;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`,h=(0,o.ZP)(c.E.h1)`
+  font-weight: bold;
+  font-size: 3.25em;
+
+  @media screen and (max-width: 768px) {
+    font-size: 2.5em;
+  }
+`,f=o.ZP.div`
+  width: min(90vw, 50rem);
+  margin: 0 auto;
+`,E=o.ZP.h2`
+  font-weight: 600;
+  font-size: 1.75em;
+  margin: 1.5rem 0;
+  text-wrap: balance;
+  @media screen and (max-width: 768px) {
+    font-size: 1.25em;
+  }
+`,y=o.ZP.label`
+  font-size: 1.5em;
+  margin: 1rem 0.75rem 0.75rem;
+  font-weight: bold;
+`,v=o.ZP.select`
+  margin: 0.5rem;
+  font-size: 1.1em;
+  padding: 0.35rem;
+  cursor: pointer;
+`}}]);
